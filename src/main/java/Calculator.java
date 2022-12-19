b346f489ba7d9f67444741cfef086690731c32cd
snehalatha1 <snehalathaps15@gmail.com>
2022-12-19 19:53:31 +0530

    firstcommit

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author sneha ganesh
+ *
+ */
+public class Calculator {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
